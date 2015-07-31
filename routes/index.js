e792ac5a7d61145e25032c117c524e25c2360623
Steven Abrams <steven.abrams86@gmail.com>
2015-07-31 14:32:03 -0600

    users can create new comics

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')(process.env.MONGOLAB_URI);
+var users = db.get('users');
+var paneCollection = db.get('panes');
+var blurbs = db.get('blurbs');
+var comics = db.get('comics');
+var helpers = require('../lib/logic');
 
-/* GET home page. */
+//INDEX (home page)
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/telecomics/new');
 });
 
+router.get('/telecomics',function (req, res, next) {
+  res.render('index');
+})
+
+//NEW
+router.get('/telecomics/new', function (req, res, next) {
+  res.render('new');
+})
+
+//CREATE
+router.post('/telecomics', function (req, res, next) {
+  var errors = helpers.validateComic(req.body);
+  if (errors.length > 0){
+    res.render("new", {errors: errors, data: req.body})
+  }
+  var panes = helpers.createPanes(req.body)
+  paneCollection.insert(panes).then(function (panes) {
+    var comic = {};
+    comic.title = req.body.title;
+    panes = panes.map(function (pane) {
+      return pane._id;
+    })
+    comic.panes = panes;
+    comic.date = Date.now();
+    comics.insert(comic).then(function (comic) {
+      res.redirect('/telecomics');
+    })
+  });
+})
 module.exports = router;
