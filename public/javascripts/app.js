16b94e0a4e15c3c339e05af1f537f1c26d710506
Steven Abrams <steven.abrams86@gmail.com>
2015-07-31 15:41:22 -0600

    users see comic created as they fill out form

---
+++ public/javascripts/app.js
@@ -0,0 +1,25 @@
+var title = document.querySelector('input')
+var panes = document.querySelectorAll('input')
+var comments = document.querySelectorAll('textarea')
+var paneRender = document.getElementsByClassName('pane')
+var commentRender = document.getElementsByClassName('comment')
+
+panes = Array.prototype.splice.call(panes,1,3)
+
+
+document.body.addEventListener('change', function (e) {
+  if (e.target.className === 'panes') {
+    var img = document.createElement('img');
+    img.src = e.target.value
+    img.className = 'paneImage'
+    console.log(e.target.id[4]);
+    paneRender[e.target.id[4] - 1].appendChild(img)
+  }
+})
+
+document.body.addEventListener('keyup', function (e) {
+  if (e.target.className === 'comments'){
+    var text = e.target.value;
+    commentRender[e.target.id[7] - 1].innerHTML = text;
+  }
+})
