cc3339c7014f9ab9c29f8376db7c33488dde41d6
Steven Abrams <steven.abrams86@gmail.com>
2015-07-31 11:58:02 -0600

    added npm dependencies

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
+var flash = require('connect-flash');
+
+require('dovenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,6 +18,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.set('trust proxy', 1) // trust first proxy
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2, process.env.SESSION_KEY3]
+}))
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -21,6 +31,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+
+
 
 app.use('/', routes);
 app.use('/users', users);
