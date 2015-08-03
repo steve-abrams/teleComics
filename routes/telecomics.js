022ba2ac032bab930208bc79951fb6e49cd7ed28
Mattias Ljung <mljung02@gmail.com>
2015-08-03 14:05:02 -0600

    sendgrid

--- routes/telecomics.js
+++ routes/telecomics.js
@@ -13,33 +13,6 @@ var bt = require('bing-translate').init({
     client_secret: process.env.TRANSLATE_SECRET
   });
 
-// bt.translate('This hotel is located close to the centre of Paris.', 'en', 'ro', function(err, res){
-//   console.log(err, res);
-// });
-//
-
-// var emailHTML = '<h1>A Ode to Parties</h1><div class="row"><div style="display: inline-block;box-sizing: border-box;overflow: hidden;width: 33%"><div style="box-sizing: border-box;overflow: hidden;margin: 5px;height: 300px;border: 1px solid black;"><img src="http://i.imgur.com/WUO7erU.jpg" style="display:block;height: 300px;width: 300px;overflow: hidden;"></div><div style="height: 100px;margin: 5px;border: 1px solid black;">No matches.</div></div><div style="display: inline-block;box-sizing: border-box;overflow: hidden;width: 33%"><div style="box-sizing: border-box;overflow: hidden;margin: 5px;height: 300px;border: 1px solid black;"><img src="http://i.imgur.com/Y3RAlLy.jpg" style="display: block;height: 100%;width: 100%;overflow: hidden;"></div><div style="height: 100px;margin: 5px;border: 1px solid black;">And the crowd.</div></div><div style="  display: inline-block;box-sizing: border-box;overflow: hidden;width: 33%"><div style="  box-sizing: border-box;overflow: hidden;margin: 5px;height: 300px;border: 1px solid black;"><img src="http://i.imgur.com/hnNFlnu.jpg" style="display: block;height: 100%;width: 100%;overflow: hidden;"></div><div style="height: 100px;margin: 5px;border: 1px solid black;">Boring days</div></div></div>'
-// var email     = new sendgrid.Email({
-// from:     'telecomics1@gmail.com',
-// subject:  'HTML TEST email',
-// text:     'view your teleocomic online at blah blah blah'
-// });x
-// 
-// 
-// email.setHtml(emailHTML);
-// 
-// email.setTos(['claar.zack@gmail.com','mljung02@gmail.com']);
-// 
-
-
-// sendgrid.send(email, function(err, json) {
-//   if (err) { return console.error(err); }
-//   console.log(json);
-// });
-// 
-
-
-
 router.get('/telecomics',function (req, res, next) {
   res.redirect('/');
 })
@@ -82,11 +55,13 @@ router.post('/telecomics/:id/send',function (req, res, next) {
       res.render('preview', {error:error, comic:comic});
     });
   }else{
+    var comicMaster = {}
     comics.findOne({_id:req.params.id}).then(function (comic) {
       var paneBlurbs = [];
       for (var i = 0; i < comic.panes.length; i++) {
         paneBlurbs.push(helpers.telephoneTranslate(comic.panes[i].comment));
       }
+      comicMaster = comic
       Promise.all(paneBlurbs).then(function (blurbs) {
 
         return transcomics.insert({comicId: req.params.id, sentTo: emails, date: Date.now(), blurbs: blurbs, owner: req.session.uId});
@@ -100,17 +75,29 @@ router.post('/telecomics/:id/send',function (req, res, next) {
           users.update({email: {$in: record.sentTo}}, {$push: {received: record._id}}, {multi: true})
         })
         users.update({_id: req.session.uId}, {$push: {sent: record._id}})
-        console.log(record)
-
-
-
+        users.findOne({_id: req.session.uId}).then(function (user) {
+          console.log('attempting email')
+          var email     = new sendgrid.Email({
+          from:     'telecomics1@gmail.com',
+          to: 'steven.abrams86@gmail.com',
+          subject:  comicMaster.title,
+          text:     'view your teleocomic online at /telecomics/' + record._id
+          });
+          // email.addTo('mljung02@gmail.com');
+          email.setHtml('<p>You have been sent a TeleComic!</p><p><a href="/telecomics/'+record._id+'">View your comic</a></p>')
+          console.log(email)
+          sendgrid.send(email, function(err, json) {
+            if (err) { return console.error(err); }
+            console.log(json);
+          });
+        })
         res.redirect('/telecomics/'+record._id);
       });
     });
   }
 });
 
-router.get('/telecomics/created', function (req, res, next) {
+router.get('/telecomics/view/created', function (req, res, next) {
   console.log(req.session.uId, '**')
   comics.find({owner_id: req.session.uId}).then(function (data) {
     res.render('created', {comics: data})
