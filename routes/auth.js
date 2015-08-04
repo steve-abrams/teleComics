9ca161751df77cf946b1de8ecd0b16c63c0af50f
Mattias Ljung <mljung02@gmail.com>
2015-08-04 11:56:36 -0600

    login validation

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var db = require('monk')(process.env.MONGOLAB_URI);
 var users = db.get('users');
 var bcrypt= require('bcrypt');
+var comics = db.get('comics');
+var transcomics = db.get('transcomics');
 
 
 router.get('/teleComics/signup', function(req, res, next) {
@@ -16,7 +18,7 @@ router.post('/teleComics/signup', function(req, res, next){
       console.log(user);
         users.update({_id: user._id}, {$set: {password: hash}})
         req.session.user=req.body.email;
-        req.session.uId=data._id;
+        req.session.uId=data._id; 
     } else {
       users.insert({email:req.body.email, password:hash});
       req.session.user=req.body.email;
@@ -29,24 +31,58 @@ router.post('/teleComics/signup', function(req, res, next){
 router.post('/teleComics/login', function(req, res, next){
   users.findOne({email:req.body.email}, function(err, data){
     if(data){
-      req.session.user=req.body.email;
-      req.session.uId=data._id
       var compare=data.password;
       var user=data.email;
       var statement;
       if (bcrypt.compareSync(req.body.password, compare)){
-        //res.cookie('currentuser', name);
+        req.session.user=req.body.email;
+        req.session.uId=data._id
         res.redirect("/");
       }else{
-        statement="Password does not match";
-        res.render("users/login", {statement:statement});
+        var comicMaster = {};
+        transcomics.find({owner: req.session.uId}).then(function (telecomics) {
+          var comicIds = telecomics.map(function (comic) {
+            return comic.comicId
+          })
+          console.log(comicIds, "telefind!")
+          var promiseArray = []
+          comicIds.forEach(function (comicId) {
+            promiseArray.push(comics.findOne({_id: comicId}))
+          })
+          comicMaster = telecomics
+          return Promise.all(promiseArray)
+        }).then(function (comicsArray) {
+          comicMaster.forEach(function (ele, i) {
+            ele.panes = comicsArray[i].panes
+          })
+          console.log(comicMaster, "comicfind")
+          statement="Password does not match";
+          res.render('users/login', {comics: comicMaster, statement:statement});
+        })    
       }
     }
     else{
-      var message="Email does not exist";
-      res.render("users/login", {message:message});
+      var comicMaster = {};
+      transcomics.find({owner: req.session.uId}).then(function (telecomics) {
+        var comicIds = telecomics.map(function (comic) {
+          return comic.comicId
+        })
+        console.log(comicIds, "telefind!")
+        var promiseArray = []
+        comicIds.forEach(function (comicId) {
+          promiseArray.push(comics.findOne({_id: comicId}))
+        })
+        comicMaster = telecomics
+        return Promise.all(promiseArray)
+      }).then(function (comicsArray) {
+        comicMaster.forEach(function (ele, i) {
+          ele.panes = comicsArray[i].panes
+        })
+        console.log(comicMaster, "comicfind")
+        var message="Email does not exist";
+        res.render('users/login', {comics: comicMaster, message:message});
+      }) 
     }
-
   });
 });
 
